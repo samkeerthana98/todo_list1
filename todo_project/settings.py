33ae6e9253072d06c16fbadaacc52a928f651bb7
samkeerthana <samkeerthana817@gmail.com>
2025-05-05 07:47:09 +0530

    Fix CSRF and set allowed host for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -18,7 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'todo-list1-1-lux5.onrender.com').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://todo-list1-1-lux5.onrender.com']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 INSTALLED_APPS = [
